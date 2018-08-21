562f9b601e3d0d84faa7ee720ef3df027682c2da
David Bramwell <david.j.bramwell@googlemail.com>
2018-08-21 15:29:45 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,4 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-// s3.listBuckets().promise()
-Promise.resolve({})
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+
