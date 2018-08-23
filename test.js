06b4932266eda9b9f1ae0172d9715aa82657ec2f
David Bramwell <david.j.bramwell@googlemail.com>
2018-08-23 14:09:13 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,2 @@
-// const s3 = new AWS.S3();
-
-// s3.listBuckets().promise()
-Promise.resolve({})
-  .then((res) => {
-    console.log(res);
-    const AWS = require('aws-sdk');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+var awslambda = require('/var/runtime/node_modules/awslambda/build/Release/awslambda');
+console.log(awslambda.initRuntime());
