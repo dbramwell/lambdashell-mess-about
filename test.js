e13ef457a3e4d19ea56bb22259dc74f66661672d
David Bramwell <david.j.bramwell@googlemail.com>
2018-08-23 14:11:17 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-var awslambda = require('/var/runtime/node_modules/awslambda/build/Release/awslambda');
+var awslambda = require('./node_modules/awslambda/build/Release/awslambda');
 console.log(awslambda.initRuntime());
